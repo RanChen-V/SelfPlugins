434cdf2fffb788a9fc546558ef7b1df24762eb85
lvjiqing <lvjiqing@playdayy.com>
2025-09-17 18:17:49 +0800

    文字弯曲效果

---
+++ TextMeshPro文字弯曲/TextProOnACircle.cs
@@ -0,0 +1,163 @@
+﻿//MIT许可证
+
+//版权所有(c) 2019 Antony Vitillo(又名 "Skarredghost")
+
+//特此免费授予任何获得本软件副本和相关文档文件（"软件"）的人
+//在软件中不受限制地处理，包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售
+//软件的副本，并允许向其提供软件的人这样做，但须符合以下条件：
+
+//上述版权声明和本许可声明应包含在软件的所有副本或主要部分中。
+
+//本软件按"原样"提供，不提供任何明示或暗示的保证，包括但不限于对适销性、
+//特定用途适用性和非侵权性的保证。在任何情况下，作者或版权持有人均不对任何索赔、损害或其他
+//责任承担责任，无论是在合同诉讼、侵权诉讼或其他诉讼中，由软件或软件的使用或其他交易引起、
+//产生或与之相关。
+
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace ntw.CurvedTextMeshPro
+{
+    /// <summary>
+    /// 用于在圆弧上绘制Text Pro文本的类
+    /// </summary>
+    [ExecuteInEditMode]
+    public class TextProOnACircle : TextProOnACurve
+    {
+        /// <summary>
+        /// 文本圆弧的半径
+        /// </summary>
+        [SerializeField]
+        [Tooltip("文本圆弧的半径")]
+        private float m_radius = 10.0f;
+
+        /// <summary>
+        /// 文本弧应该跨越多少度
+        /// </summary>
+        [SerializeField]
+        [Tooltip("文本弧应该跨越多少度")]
+        private float m_arcDegrees = 90.0f;
+
+        /// <summary>
+        /// 弧应该居中的角度偏移，以度为单位。
+        /// -90度意味着文本在最高点居中
+        /// </summary>
+        [SerializeField]
+        [Tooltip("弧应该居中的角度偏移，以度为单位")]
+        private float m_angularOffset = -90;
+
+        /// <summary>
+        /// 每个字母的最大度数应该是多少。例如，如果您指定
+        /// 10度，字母之间的距离永远不会超过10度。
+        /// 这对于创建优雅地展开直到达到完整弧的文本很有用，
+        /// 当字符串较短时不会使字母过于稀疏
+        /// </summary>
+        [SerializeField]
+        [Tooltip("字母之间的最大角度距离，以度为单位")]
+        private int m_maxDegreesPerLetter = 360;
+
+        /// <summary>
+        /// <see cref="m_radius"/>的前一个值
+        /// </summary>
+        private float m_oldRadius = float.MaxValue;
+
+        /// <summary>
+        /// <see cref="m_arcDegrees"/>的前一个值
+        /// </summary>
+        private float m_oldArcDegrees = float.MaxValue;
+
+        /// <summary>
+        /// <see cref="m_angularOffset"/>的前一个值
+        /// </summary>
+        private float m_oldAngularOffset = float.MaxValue;
+
+        /// <summary>
+        /// <see cref="m_maxDegreesPerLetter"/>的前一个值
+        /// </summary>
+        private float m_oldMaxDegreesPerLetter = float.MaxValue;
+
+        /// <summary>
+        /// 每帧执行的方法，检查某些参数是否已更改
+        /// </summary>
+        /// <returns></returns>
+        protected override bool ParametersHaveChanged()
+        {
+            
+            //检查参数是否已更改并更新旧值以供下一帧迭代使用 废弃判断:|| m_oldMaxDegreesPerLetter != m_maxDegreesPerLetter
+            bool retVal = m_radius != m_oldRadius || m_arcDegrees != m_oldArcDegrees || m_angularOffset != m_oldAngularOffset;
+
+            m_oldRadius = m_radius;
+            m_oldArcDegrees = m_arcDegrees;
+            m_oldAngularOffset = m_angularOffset;
+            m_oldMaxDegreesPerLetter = m_maxDegreesPerLetter;
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// 计算变换矩阵，该矩阵将每个单个字符的顶点从字符中心到顶点的最终目标位置的偏移量映射，
+        /// 使文本遵循曲线
+        /// </summary>
+        /// <param name="charMidBaselinePos">字符中心点的位置</param>
+        /// <param name="zeroToOnePos">字符相对于框边界的水平位置，范围[0, 1]</param>
+        /// <param name="textInfo">我们正在显示的文本信息</param>
+        /// <param name="charIdx">我们必须计算变换的字符索引</param>
+        /// <returns>要应用于文本所有顶点的变换矩阵</returns>
+        protected override Matrix4x4 ComputeTransformationMatrix(Vector3 charMidBaselinePos, float zeroToOnePos, TMP_TextInfo textInfo, int charIdx)
+        {
+            Debug.LogError(textInfo.characterInfo[charIdx].character + "字符的边框位置:" + zeroToOnePos + ",中心点位置:" + charMidBaselinePos);
+            var rectTrans = textInfo.textComponent.rectTransform;
+
+            float totalHeight = 0;
+            float curLineHeight = 0;
+            var lineIndex = textInfo.characterInfo[charIdx].lineNumber;
+            for (var i = 1; i < textInfo.lineInfo.Length; i++)
+            {
+                var lineInfo = textInfo.lineInfo[i];
+                if (lineInfo.characterCount <= 0) break;
+                totalHeight += lineInfo.lineHeight;
+                if (i <= lineIndex) curLineHeight += lineInfo.lineHeight;
+            }
+
+            float boundsMaxX = textInfo.lineInfo[lineIndex].lineExtents.max.x;
+            float boundsMinX = textInfo.lineInfo[lineIndex].lineExtents.min.x;
+            
+            zeroToOnePos = (charMidBaselinePos.x - boundsMinX) / (boundsMaxX - boundsMinX);
+            
+            float lastPrecent = (textInfo.lineInfo[lineIndex].length % rectTrans.rect.width) / rectTrans.rect.width;
+            float actualArcDegrees = lastPrecent * m_arcDegrees;
+            // m_maxDegreesPerLetter = (int)(actualArcDegrees / ((float)textInfo.characterCount / textInfo.lineCount));
+            //计算考虑字母之间最大距离的弧的实际度数
+            //float actualArcDegrees = Mathf.Min(m_arcDegrees, (float)textInfo.characterCount / textInfo.lineCount * m_maxDegreesPerLetter);
+
+            //计算显示此字符的角度。
+            //我们希望字符串在圆的最高点居中，所以我们首先将位置从范围[0, 1]转换为[-0.5, 0.5]，
+            //然后添加m_angularOffset度，使其在所需点居中
+            float angle = ((zeroToOnePos - 0.5f) * actualArcDegrees + m_angularOffset) * Mathf.Deg2Rad; //我们需要弧度用于sin和cos
+            
+            //计算字符中心点新位置的坐标。使用sin和cos，因为我们在圆上。
+            //注意，我们必须进行一些额外的计算，因为我们必须考虑到文本可能在多行上
+            float x0 = Mathf.Cos(angle);
+            float y0 = Mathf.Sin(angle);//(textInfo.lineInfo[0].lineExtents.max.y - textInfo.lineInfo[0].lineExtents.min.y)
+            float radiusForThisLine =
+                m_radius - curLineHeight;
+            // Vector2 newMideBaselinePos = new Vector2(x0 * radiusForThisLine,
+            //     -y0 * radiusForThisLine - Mathf.Abs(m_radius * Mathf.Cos(actualArcDegrees*Mathf.Deg2Rad)) + textInfo.textComponent.rectTransform.rect.height/2); //字符的实际新位置
+
+            Vector2 newMideBaselinePos = new Vector2(x0 * radiusForThisLine,
+                -y0 * radiusForThisLine - m_radius + totalHeight); //字符的实际新位置
+            
+            //计算变换矩阵：将点移动到刚找到的位置，然后旋转字符以适合曲线的角度
+            //(-90是因为文本已经是垂直的，就好像它已经旋转了90度一样)
+            return Matrix4x4.TRS(new Vector3(newMideBaselinePos.x, newMideBaselinePos.y, 0), Quaternion.AngleAxis(-Mathf.Atan2(y0, x0) * Mathf.Rad2Deg - 90, Vector3.forward), Vector3.one);
+        }
+
+        protected override void Calculate(TMP_TextInfo textInfo)
+        {
+            var lineLength = textInfo.lineInfo[0].width;
+            var deg = m_arcDegrees * Mathf.Deg2Rad;
+            m_radius = lineLength / deg;
+        }
+    }
+}
---
+++ TextMeshPro文字弯曲/TextProOnACurve.cs
@@ -0,0 +1,155 @@
+﻿//MIT 许可证
+
+//版权所有(c) 2019 Antony Vitillo(又名 "Skarredghost")
+
+//特此免费授予任何获得本软件副本和相关文档文件（"软件"）的人，
+//不受限制地处理本软件，包括但不限于使用、复制、修改、合并、发布、
+//分发、再许可和/或销售软件副本的权利，并允许向其提供软件的人这样做，
+//但须符合以下条件：
+
+//上述版权声明和本许可声明应包含在软件的所有副本或主要部分中。
+
+//本软件按"原样"提供，不提供任何明示或暗示的保证，包括但不限于
+//对适销性、特定用途适用性和非侵权性的保证。在任何情况下，作者或
+//版权持有人均不对因软件或软件使用或其他交易而产生的任何索赔、损害
+//或其他责任承担责任，无论是在合同诉讼、侵权诉讼或其他诉讼中。
+
+//代码灵感来源于TextMeshPro包中的WarpTextExample
+
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using System;
+
+namespace ntw.CurvedTextMeshPro
+{
+    /// <summary>
+    /// 用于绘制沿特定曲线排列的Text Pro文本的基类
+    /// </summary>
+    [ExecuteInEditMode]
+    public abstract class TextProOnACurve : MonoBehaviour
+    {
+        /// <summary>
+        /// 感兴趣的文本组件
+        /// </summary>
+        private TMP_Text m_TextComponent;
+
+        /// <summary>
+        /// 如果文本必须在此帧更新则为真
+        /// </summary>
+        private bool m_forceUpdate;
+
+        /// <summary>
+        /// Awake
+        /// </summary>
+        private void Awake()
+        {
+            m_TextComponent = gameObject.GetComponent<TMP_Text>();
+        }
+
+        /// <summary>
+        /// OnEnable
+        /// </summary>
+        private void OnEnable()
+        {
+            //每次对象被启用时，我们必须强制重新创建文本网格
+            m_forceUpdate = true;
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        protected void Update()
+        {
+            //如果文本和参数与上一帧相同，不要浪费时间重新计算所有内容
+            if (!m_forceUpdate && !m_TextComponent.havePropertiesChanged && !ParametersHaveChanged())
+            {
+                return;
+            }
+
+            m_forceUpdate = false;
+
+            //在循环中，vertices表示我们正在分析的单个字符的4个顶点，
+            //而matrix是旋转平移矩阵，它将旋转和缩放字符，使它们能够跟随曲线
+            Vector3[] vertices;
+            Matrix4x4 matrix;
+
+            //生成网格并获取关于文本和字符的信息
+            m_TextComponent.ForceMeshUpdate();
+
+            TMP_TextInfo textInfo = m_TextComponent.textInfo;
+            int characterCount = textInfo.characterCount;
+
+            //如果字符串为空，无需浪费时间
+            if (characterCount == 0)
+                return;
+
+            Calculate(textInfo);
+            //获取包含文本的矩形的边界
+            float boundsMinX = m_TextComponent.bounds.min.x;
+            float boundsMaxX = m_TextComponent.bounds.max.x;
+
+            //对于每个字符
+            for (int i = 0; i < characterCount; i++)
+            {
+                //跳过不可见的字符
+                if (!textInfo.characterInfo[i].isVisible)
+                    continue;
+
+                //获取此字符使用的网格索引，然后是材质的索引...并使用所有这些数据获取
+                //包围此字符的矩形的4个顶点。将它们存储在vertices中
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+                int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+                vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                //计算每个字符的基线中点。这是字符的中心点。
+                //我们将使用它作为表示此字符的几何变换点
+                Vector3 charMidBaselinePos = new Vector2((vertices[vertexIndex + 0].x + vertices[vertexIndex + 2].x) / 2, textInfo.characterInfo[i].baseLine);
+
+                //从顶点位置中移除中心点。在此操作之后，四个顶点中的每一个
+                //将只具有相对于中心位置的偏移坐标。这在处理旋转时会很有用
+                vertices[vertexIndex + 0] += -charMidBaselinePos;
+                vertices[vertexIndex + 1] += -charMidBaselinePos;
+                vertices[vertexIndex + 2] += -charMidBaselinePos;
+                vertices[vertexIndex + 3] += -charMidBaselinePos;
+
+                // //计算字符相对于框边界的水平位置，范围在[0, 1]内，
+                // //其中0是文本的左边界，1是右边界
+                // 因为存在多行的情况 需要根据当前字符所在行进行计算 将代码移入矩阵计算公式
+                // float zeroToOnePos = (charMidBaselinePos.x - boundsMinX) / (boundsMaxX - boundsMinX);
+
+                //获取变换矩阵，该矩阵将顶点（视为相对于字符中心点的偏移）映射到它们的最终位置，
+                //使文本能够跟随曲线
+                matrix = ComputeTransformationMatrix(charMidBaselinePos, 0, textInfo, i);
+
+                //应用变换，并获得表示此字符的4个顶点的最终位置和方向
+                vertices[vertexIndex + 0] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 0]);
+                vertices[vertexIndex + 1] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 1]);
+                vertices[vertexIndex + 2] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 2]);
+                vertices[vertexIndex + 3] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 3]);
+            }
+
+            //使用修订后的信息上传网格
+            m_TextComponent.UpdateVertexData();
+        }
+
+        /// <summary>
+        /// 在每一帧执行的方法，检查某些参数是否已更改
+        /// </summary>
+        /// <returns></returns>
+        protected abstract bool ParametersHaveChanged();
+
+        /// <summary>
+        /// 计算变换矩阵，该矩阵将每个单个字符的顶点相对于字符中心的偏移映射到
+        /// 顶点的最终目标位置，使文本能够跟随曲线
+        /// </summary>
+        /// <param name="charMidBaselinePosfloat">字符中心点的位置</param>
+        /// <param name="zeroToOnePos">字符相对于框边界的水平位置，范围在[0, 1]内</param>
+        /// <param name="textInfo">我们正在显示的文本信息</param>
+        /// <param name="charIdx">我们必须为其计算变换的字符索引</param>
+        /// <returns>要应用于文本所有顶点的变换矩阵</returns>
+        protected abstract Matrix4x4 ComputeTransformationMatrix(Vector3 charMidBaselinePos, float zeroToOnePos, TMP_TextInfo textInfo, int charIdx);
+
+        protected abstract void Calculate(TMP_TextInfo textInfo);
+    }
+}
